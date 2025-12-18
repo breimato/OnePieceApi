0d0c5440ef983aafa86d321a44c871cffe869cbb
BreiMato <breimatom@gmail.com>
2025-12-18 23:04:37 +0100

    Complete POST characters

---
+++ web/src/main/java/es/api/onepiece/core/internal/vo/character/CreateCharacterAffiliationVo.java
@@ -0,0 +1,26 @@
+package es.api.onepiece.core.internal.vo.character;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * The Class CreateCharacterAffiliationVo.
+ */
+@Builder
+@AllArgsConstructor
+@Data
+public class CreateCharacterAffiliationVo {
+
+    /** The affiliation id. */
+    @NotNull
+    private Integer affiliationId;
+
+    /** The status id. */
+    @NotNull
+    private Integer statusId;
+
+    /** The role id. */
+    private Integer roleId;
+}
