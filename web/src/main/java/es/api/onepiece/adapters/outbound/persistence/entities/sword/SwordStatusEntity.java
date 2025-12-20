2827c2dab35c2dd17c3244edd63feaa2650cb277
BreiMato <breimatom@gmail.com>
2025-12-20 05:37:39 +0100

    Attack, Transformation & Sword CRUD
    TODO: Error msg when one refers to another and it does not exist.
    TODO: Sword tests
    TODO: exists method called by persistence port instead of mybatis directly in all 3 CRUDs
    
    Review and test deeper to find more

---
+++ web/src/main/java/es/api/onepiece/adapters/outbound/persistence/entities/sword/SwordStatusEntity.java
@@ -0,0 +1,32 @@
+package es.api.onepiece.adapters.outbound.persistence.entities.sword;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * The Class SwordStatusEntity.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SwordStatusEntity implements Serializable {
+
+    /** The Constant serialVersionUID. */
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /** The id. */
+    @NotNull
+    private Integer id;
+
+    /** The name. */
+    @NotNull
+    private String name;
+}
